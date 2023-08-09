a626b8a420f02a816e6398a438e600f653ef456b
Aditya Kumar <adityapal1409@gmail.com>
2023-08-09 17:34:31 +0530

    add icons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,115 +1,48 @@
+"use client";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "./logo.ico";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+	const router = useRouter();
 	return (
 		<nav className='bg-gray-800'>
-			<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-				<div className='relative flex h-16 items-center justify-between'>
-					<div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
-						{/* <!-- Mobile menu button--> */}
-						<button
-							type='button'
-							className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
-							aria-controls='mobile-menu'
-							aria-expanded='false'>
-							<span className='absolute -inset-0.5'></span>
-							<span className='sr-only'>Open main menu</span>
-							{/* <!--
-  Icon when menu is closed.
-
-  Menu open: "hidden", Menu closed: "block"
---> */}
-							<svg
-								className='block h-6 w-6'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke-width='1.5'
-								stroke='currentColor'
-								aria-hidden='true'>
-								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-								/>
-							</svg>
-							{/* <!--
-  Icon when menu is open.
-
-  Menu open: "block", Menu closed: "hidden"
---> */}
-							<svg
-								className='hidden h-6 w-6'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke-width='1.5'
-								stroke='currentColor'
-								aria-hidden='true'>
-								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									d='M6 18L18 6M6 6l12 12'
-								/>
-							</svg>
-						</button>
-					</div>
-					<div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
-						<div className='flex flex-shrink-0 items-center'>
-							<img
-								className='h-8 w-auto'
-								src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-								alt='Your Company'
-							/>
-						</div>
-					</div>
-					<div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-						<div className='hidden sm:ml-6 sm:block'>
-							<div className='flex space-x-4'>
-								{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-
-								<Link
-									href='/'
-									className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>
-									Home
-								</Link>
-								<Link
-									href='/privacy'
-									className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>
-									Privacy
-								</Link>
-								<a
-									href='#'
-									className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>
-									Calendar
-								</a>
-							</div>
-						</div>
+			<div className='relative flex h-16 items-center justify-center'>
+				<div className='flex flex-1 sm:items-stretch sm:justify-start ml-2.5'>
+					<div className='flex flex-shrink-0 items-center'>
+						<Image
+							className='h-8 w-auto'
+							src={logo}
+							alt='Your Company'
+							onClick={() => router.push("/")}
+						/>
+						<p className='hidden sm:block m-2 font-semibold text-gray-300'>
+							Color Picker
+						</p>
 					</div>
 				</div>
-			</div>
-			{/* <!-- Mobile menu, show/hide based on menu state. --> */}
-			<div className='sm:hidden' id='mobile-menu'>
-				<div className='space-y-1 px-2 pb-3 pt-2'>
-					<a
-						href='#'
-						className='bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium'
-						aria-current='page'>
-						Dashboard
-					</a>
-					<a
-						href='#'
-						className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'>
-						Team
-					</a>
-					<a
-						href='#'
-						className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'>
-						Projects
-					</a>
-					<a
-						href='#'
-						className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'>
-						Calendar
-					</a>
+				<div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 mr-2.5'>
+					<div className='sm:ml-6 sm:block'>
+						<div className='flex space-x-4'>
+							{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+							<Link
+								href='/'
+								className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>
+								Home
+							</Link>
+							<Link
+								href='/privacy'
+								className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>
+								Privacy
+							</Link>
+							<a
+								href='/about'
+								className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>
+								About
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</nav>
